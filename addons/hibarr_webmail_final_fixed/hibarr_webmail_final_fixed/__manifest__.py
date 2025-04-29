308538aba4de6947a8a6c7d0f41d11ae8faf8b82
ChrisF93 <157891902+ChrisF93@users.noreply.github.com>
2025-04-29 12:12:27 +0300

    Webmail on SocketSSL Fixed

---
+++ addons/hibarr_webmail_final_fixed/hibarr_webmail_final_fixed/__manifest__.py
@@ -0,0 +1,13 @@
+
+{
+    "name": "HIBARR Webmail",
+    "version": "1.0",
+    "depends": ["base"],
+    "author": "HIBARR",
+    "category": "Tools",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/mail_account_views.xml"
+    ],
+    "application": True
+}
