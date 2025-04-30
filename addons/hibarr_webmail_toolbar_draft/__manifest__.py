3f69c6594b1605d2faec607f833be0ddfead200d
ChrisF93 <157891902+ChrisF93@users.noreply.github.com>
2025-04-30 10:47:52 +0300

    View Webmail

---
+++ addons/hibarr_webmail_toolbar_draft/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "HIBARR Webmail Toolbar Draft",
+    "version": "1.0",
+    "summary": "Draft for toolbar with filter, search, and sort in webmail",
+    "category": "Tools",
+    "author": "HIBARR",
+    "depends": ["base"],
+    "data": [
+        "views/webmail_toolbar_view.xml"
+    ],
+    "installable": True,
+    "application": False
+}
