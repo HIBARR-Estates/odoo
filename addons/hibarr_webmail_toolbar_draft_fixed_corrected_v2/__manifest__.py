96687dbccdf088f7949289101c88256c96cbe917
ChrisF93 <157891902+ChrisF93@users.noreply.github.com>
2025-05-01 10:51:14 +0300

    view update

---
+++ addons/hibarr_webmail_toolbar_draft_fixed_corrected_v2/__manifest__.py
@@ -0,0 +1,9 @@
+
+{
+    "name": "HIBARR Webmail Toolbar Draft Fixed V2",
+    "version": "1.0",
+    "depends": ["base"],
+    "data": ["views/webmail_message_views.xml"],
+    "installable": True,
+    "auto_install": False
+}
