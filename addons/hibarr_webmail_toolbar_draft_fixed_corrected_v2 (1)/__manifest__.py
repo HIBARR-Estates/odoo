71fb9ffed52850c95d11038a294fb441763650d8
ChrisF93 <157891902+ChrisF93@users.noreply.github.com>
2025-05-01 11:05:08 +0300

    v2 view webmail

---
+++ addons/hibarr_webmail_toolbar_draft_fixed_corrected_v2 (1)/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "HIBARR Webmail Toolbar Draft Fixed",
+    "version": "1.0",
+    "category": "Tools",
+    "summary": "Draft toolbar and message view fix for webmail module",
+    "depends": ["base"],
+    "data": [
+        "views/webmail_message_views.xml"
+    ],
+    "installable": True,
+    "application": False,
+}
