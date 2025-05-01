712e3d0e79eb337911a0ce323766cc8805f294bf
ChrisF93 <157891902+ChrisF93@users.noreply.github.com>
2025-05-01 11:29:57 +0300

    webmail view v3

---
+++ addons/hibarr_webmail_toolbar_draft_corrected_final/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "HIBARR Webmail Toolbar Draft Corrected Final",
+    "version": "1.0",
+    "category": "Tools",
+    "summary": "Webmail module with corrected external IDs",
+    "author": "HIBARR",
+    "depends": ["base"],
+    "data": ["views/webmail_message_views.xml"],
+    "installable": True,
+    "application": True
+}
